8f5abe43f272b42e655bcc0243d96163a9757b15
Matthew Yu <myu@cpp.edu>
2022-10-22 22:01:03 -0700

    Fixed tax/tip

--- Restaurant.c
+++ Restaurant.c
@@ -8,14 +8,16 @@ int main(int argc, char *argv[]){
     double price[4] = {9.95, 4.55, 13.25, 22.35};
     long tax = argv[1];
     long tip = argv[2];
+    long tax1 = tax/100;
+    long tip1 = tip/100;
     int r = rand() % 4;
     char item = name[r];
     double price1 = price[r];
     double test = price[r] * tip;
     printf("The meal cost is %d\n", price1);
-    printf("The tip amount is %d\n", price1 * tip);
-    printf("The tax amount is %d\n", price1 * tax);
-    printf("The total meal is %d\n", price1 + price1 * tip + price1 * tax);
+    printf("The tip amount is %d\n", price1 * tip1);
+    printf("The tax amount is %d\n", price1 * tax1);
+    printf("The total meal is %d\n", price1 + price1 * tip1 + price1 * tax1);
 
     
     
